c582dad75e32a216d903cecea02b59fc0b3f33d9
ronnicrafter <aroncarllenzen@gmx.de>
2024-05-03 19:00:43 +0200

    r

--- TTT/ttt.py
+++ TTT/ttt.py
@@ -4,18 +4,30 @@ class TTT(arcade.Window):
     def __init__ (self): # diese funktion heißt konstruktur und wird automatisch dann aufgerufen, wenn ein objekt der klasse TTT erstellt wird 
         super().__init__(600, 600, "Tic Tac Toe") # das fenster soll 600x600 pixel groß sein und den titel "tic tac toe" besitzen 
 
-        self.arena = { (0, 0): "X", (1, 0): "", (2, 0): "", (0, 1): "", (1, 1): "", (2, 1): "O", (0, 2): "", (1, 2): "", (2, 2): ""}
+        self.arena = { (0, 0): "", (1, 0): "", (2, 0): "", (0, 1): "", (1, 1): "", (2, 1): "", (0, 2): "", (1, 2): "", (2, 2): ""}
+
+        self.spieler = "X"
+
+    # Wird automatisch immer dann ausgeführt, wenn der Spieler mit der Maus klickt 
+    # In x, y steht Position des Mausklicks
 
     def on_mouse_press(self, x, y, button, modifiers):
-        for self.koordinate in self.arena:
-  
+    
+        x_arena = x // 200 # Durch 200 teilen und abrunden/alle Nachkommastellen abschneiden
+        y_arena = y // 200
+        # 0 <= x < 200 -> 0
+        # 200 <= x < 400 -> 1
+        # 400 <= x < 600 -> 2
+        self.arena[(x_arena ,y_arena )] = self.spieler
 
+        self.spieler = "O" if self.spieler == "X" else "x"
     def on_update(self, delta_time):
         ...
 
     def on_draw(self):
         self.clear()
 
+        #Eine Weiße Linie der Dicke 12 von (x=20, y=200, x=580, y=200)
         arcade.draw_line(20, 200, 580, 200, arcade.color.WHITE, 12)
         arcade.draw_line(20, 400, 580, 400, arcade.color.WHITE, 12)
         arcade.draw_line(200, 580, 200, 20, arcade.color.WHITE, 12)
